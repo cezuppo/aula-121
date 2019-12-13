776422bc71a0a0a4c2ab115d896224f7cadc6127
cezuppo <czuppo@gmail.com>
2019-12-13 07:57:14 -0300

    Saidas do programa mais detalhadas.

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,28 +28,28 @@ public class Program {
 		if (acc3 instanceof BusinessAccount) {
 			BusinessAccount acc5 = (BusinessAccount)acc3;
 			acc5.loan(200.0);
-			System.out.println("Loan!");
+			System.out.println("Anna has a Business Account. Balance after loan: " + acc5.getBalance());
 		}
 		
 		if (acc3 instanceof SavingsAccount) {
 			SavingsAccount acc5 = (SavingsAccount)acc3;
 			acc5.updateBalance();
-			System.out.println("Update!");
+			System.out.println("Anna has a Savings Account. Balance: " + acc5.getBalance());
 		}
 		
 		//SUPERPOSITION
 		
 		Account acc6 = new Account(1005, "Brian", 1000.0);
 		acc6.withdraw(200.0);
-		System.out.println("Brian's Balance: " + acc6.getBalance());
+		System.out.println("Brian's account balance after withdraw: " + acc6.getBalance());
 		
 		Account acc7 = new SavingsAccount(1006, "Amanda", 1000.0, 0.01);
 		acc7.withdraw(200.0);
-		System.out.println("Amanda's Balance: " + acc7.getBalance());
+		System.out.println("Amanda's savings account balance after withdraw: " + acc7.getBalance());
 		
 		Account acc8 = new BusinessAccount(1007, "Chris", 1000.0, 500.0);
 		acc8.withdraw(200.0);
-		System.out.println("Chris' Balance: " + acc8.getBalance());
+		System.out.println("Chris' business account balance after withdraw: " + acc8.getBalance());
 	}
 	
 	
